a37d0830d69fd3ef359f86ee82fe25eefa4d810e
prashant jugran <prashantjugran@prashants-MacBook-Pro.local>
2020-11-11 09:55:46 +0530

    create structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PaymentComponent } from './payment/payment.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path:'home', loadChildren:'./home/home.module#HomeModule'
+  },
+  {
+    path:'payment', loadChildren:'./payment/payment.module#PaymentModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
